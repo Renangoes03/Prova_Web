dc860b83c3ab8578d83ecd52d0a1e2bec18523e6
Renangoes03~ <renangoessantos9@gmail.com>
2023-04-11 10:21:03 -0300

    quinto commit

--- src/app/conversor/conversor.component.ts
+++ src/app/conversor/conversor.component.ts
@@ -10,27 +10,26 @@ export class ConversorComponent {
   real: number = 0;
   euro: number = 0;
   peso: number = 0;
-afegao: number = 0;
-ariay:  number = 0;
-  converterParaReal(): void {
-    this.dolar = this.real *  5.72 ;
-  }
+  afegao: number = 0;
+  ariay:  number = 0;
 
+  converterParaReal(): void {
+    this.dolar = this.real *  0.20;
+  }
   converterParaDolar() {
-    this.real = this.dolar *  0.20;
+    this.real = this.dolar *  5.03;
+    this.real = this.dolar /  5.03;
   }
 
   converterParaEuro(): void {
-    this.peso = this.euro *  0.18 ;
+    this.peso = this.euro *  5.46 ;
   }
   converterParaPeso(): void {
-    this.euro = this.peso *  0.0043 ;
-  }
-  
+    this.euro = this.peso *  0.23 ;
+  } 
   converterParaAfegao(): void {
     this.ariay = this.afegao *  1 ;
-  }
-  
+  } 
   converterParaAriay(): void {
     this.afegao = this.ariay *  50.43 ;
   }
